8bf9a6dbaec001bcb376b97ad574ca1f0cb66bee
markd315 <markd315@gmail.com>
2018-08-02 01:05:43 -0400

    Set up dependencies and main class structure

--- src/main/ThreeDamned.java
+++ src/main/ThreeDamned.java
@@ -1,5 +1,34 @@
 package main;
 
+import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class ThreeDamned {
 
+	private String[] blacklist;
+	
+	public static String toSHA1(byte[] convertme) {
+	    MessageDigest md = null;
+	    try {
+	        md = MessageDigest.getInstance("SHA-1");
+	    }
+	    catch(NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	    } 
+	    return new String(md.digest(convertme));
+	}
+	
+	public boolean compareAgainstBlacklist(File stl) {
+		return false;
+		//TODO unimplemented
+	}
+	
+	
+	
+	static {
+		//TODO load blacklist into memory
+		JSONObjectMapper mapper = new JSONObjectMapper();
+	}
+	
 }
