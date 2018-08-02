4d0741174a674a3d81008a13c3ddf4f5957cb435
markd315 <markd315@gmail.com>
2018-08-02 00:49:46 -0400

    Initial repo and class setup

---
+++ src/test/WrongHashesNotBlocked.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class WrongHashesNotBlocked {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
